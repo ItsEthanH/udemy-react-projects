db81d1e38443561b587cb00d7ecd96c4ab00c29b
Ethan <89361954+ItsEthanH@users.noreply.github.com>
2022-06-07 18:49:44 +0100

    learning more router

--- learning-react-router/src/App.js
+++ learning-react-router/src/App.js
@@ -1,16 +1,27 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+
+import MainHeader from './components/MainHeader';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
