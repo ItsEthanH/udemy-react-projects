494b5e97749d821fc4c16c75f2c6f1fcd0c18181
Ethan Hawes <ethanhawes@outlook.com>
2022-07-08 15:13:21 +0100

    finished authentication module

--- authentication-with-jwts/src/components/Auth/AuthForm.js
+++ authentication-with-jwts/src/components/Auth/AuthForm.js
@@ -1,30 +1,108 @@
-import { useState } from 'react';
+import { useState, useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import AuthContext from '../../context/authContext';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const history = useHistory();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const authCtx = useContext(AuthContext);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  function submitHandler(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // theoretically, validation would happen here. that ain't the focus of this lesson though ;)
+    let url;
+    if (isLogin) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBzoUrJJKpCvX0AlhAzscsoIBD3pZ4eKr0';
+    } else {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBzoUrJJKpCvX0AlhAzscsoIBD3pZ4eKr0';
+    }
+
+    async function sendSignUpRequest() {
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const data = await response.json();
+
+      setIsLoading(false);
+      if (!response.ok) {
+        let errorMessage =
+          'Registration failed! Please ensure all fields are valid and try again!';
+
+        if (data && data.error && data.error.message) {
+          errorMessage = data.error.message;
+        }
+
+        throw new Error(errorMessage);
+      }
+
+      return data;
+    }
+
+    sendSignUpRequest()
+      .then((data) => {
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+
+        authCtx.login(data.idToken, expirationTime.toISOString());
+        history.replace('/');
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  }
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" ref={emailInputRef} required />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            ref={passwordInputRef}
+            required
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && (
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
