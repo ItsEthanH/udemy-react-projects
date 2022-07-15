19d0b5d781ac5a2d76c99106281b083c2c2d0ac1
Ethan Hawes <ethanhawes@outlook.com>
2022-07-15 13:23:28 +0100

    more next learning

---
+++ nextjs-crash-course/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      title="A First Meetup"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Buildings-1804479_1920.jpg/1024px-Buildings-1804479_1920.jpg"
+      description="A first meetup"
+      address="Some St, Number 5, City, State, PO57 CO53"
+    />
+  );
+}
+
+export default MeetupDetails;
---
+++ nextjs-crash-course/pages/index.js
@@ -0,0 +1,32 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Buildings-1804479_1920.jpg/1024px-Buildings-1804479_1920.jpg',
+    address: 'Sample Street, City, State, PO57 C053',
+  },
+  {
+    id: 'm2',
+    title: 'Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Buildings-1804479_1920.jpg/1024px-Buildings-1804479_1920.jpg',
+    address: 'Sample Street, City, State, PO57 C053',
+  },
+];
+
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+}
+
+export default HomePage;
