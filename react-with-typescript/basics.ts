ce68318c4f3debf92beb17bbca7be27792c1df00
Ethan <89361954+ItsEthanH@users.noreply.github.com>
2022-07-18 17:59:54 +0100

    beginning react-and-typescript module

---
+++ react-with-typescript/basics.ts
@@ -0,0 +1,24 @@
+// Primitives: number, string, boolean
+let age: number = 24;
+age = 12;
+
+let userName: string;
+userName = 'Ethan';
+
+let isAuthenticated: boolean = false;
+isAuthenticated = true;
+
+let course: string | number = 'Introduction to Javascript';
+course = 221011; // Example: course name and id allowed
+
+// Complex: arrays, objects
+let hobbies: string[];
+hobbies = ['Sports', 'Music', 'Coding'];
+
+let person: { name: string; age: number };
+person = {
+  name: 'Ethan',
+  age: 21,
+};
+
+// Function types & parameters
