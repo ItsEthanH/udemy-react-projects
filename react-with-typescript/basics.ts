ac35711d0e1bc1eb8a9af47a365027c66bf28678
Ethan <89361954+ItsEthanH@users.noreply.github.com>
2022-07-18 22:05:23 +0100

    finished introduction to typescript

--- react-with-typescript/basics.ts
+++ react-with-typescript/basics.ts
@@ -15,10 +15,29 @@ course = 221011; // Example: course name and id allowed
 let hobbies: string[];
 hobbies = ['Sports', 'Music', 'Coding'];
 
-let person: { name: string; age: number };
+type Person = { name: string; age: number };
+let person: Person;
 person = {
   name: 'Ethan',
   age: 21,
 };
 
+let people: Person[];
+
 // Function types & parameters
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function print(value: any) {
+  console.log(value);
+}
+
+// Generics - write type-safe, yet flexible, functions
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
